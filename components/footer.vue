5e36ab0665655ea42564debf237e59144d63619f
Ryutaro Seki <dragonproject2002@gmail.com>
2023-11-08 10:08:18 +0900

    finish before deployment

---
+++ components/footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer>
+    <div class="insideContainer">
+      <a href="https://www.google.com"><img src="/images/github.svg" alt="logo" /></a>
+      <a href="https://www.google.com"><img src="/images/linkedin.svg" alt="logo" /></a>
+      <a href="https://www.google.com"><img src="/images/twitter.svg" alt="logo" /></a>
+      <a href="https://www.google.com"><img src="/images/facebook.svg" alt="logo" /></a>
+      <hr />
+      <NuxtLink to="blog">Blog</NuxtLink>
+      <p>&copy;{{ new Date().getFullYear() }}Abe Hiroki</p>
+    </div>
+  </footer>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
+  modules:[
+    "@nuxt/content",
+    "@nuxt/image"
+  ],
+  css:[
+    "~/assets/styles/common.css",
+    "~/assets/styles/layout.css",
+    "~/assets/styles/index.css",
+    "~/assets/styles/blog.css",
+    "~/assets/styles/contact.css",
+    "~/assets/styles/singleBlog.css",
+  ]
+})
